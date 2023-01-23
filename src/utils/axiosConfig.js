b03bb01baa3ea7f44e880b8ddb3836fa8be7f542
Rajender Pandey <rajendra.pandey21@gmail.com>
2023-01-24 01:10:18 +0530

    changed todo routes endpoint

--- src/utils/axiosConfig.js
+++ src/utils/axiosConfig.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: "http://localhost:5000", //"https://todo-app-mern.herokuapp.com",
+  baseURL: "https://todo-app-mern.herokuapp.com",
 });
 
 export default instance;
